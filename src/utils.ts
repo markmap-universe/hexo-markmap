573e62450948312692a73963863f0a04a5ba3577
MaxChang3 <maxcfoo@gmail.com>
2025-03-19 19:20:27 +0800

    chore: format

--- src/utils.ts
+++ src/utils.ts
@@ -24,8 +24,8 @@ const frontmatterSchema = z.object({
  * Generate a short id from an identifier.
  * @param identifier 
  */
-export const generateShortId = (identifier: string) => 
-        `hmm-${xxh64(identifier).toString(16).slice(0, 8)}`
+export const generateShortId = (identifier: string) =>
+    `hmm-${xxh64(identifier).toString(16).slice(0, 8)}`
 
 
 /**
